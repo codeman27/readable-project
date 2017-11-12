67c7673ecefb9ee2289c3c02e6b77f7407f35a52
codeman27 <codeman2727@gmail.com>
2017-11-12 12:25:33 +0000

    Attempt another turn in.

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,51 +1,42 @@
-import React, { Component } from 'react'
+import React from 'react'
 import backArrow from '../Images/BackArrow.svg'
 import { Link } from 'react-router-dom'
 import arrow from '../Images/Arrow.svg'
 import editSymbol from '../Images/EditSymbol.svg'
 import addPostImg from '../Images/AddSymbol.svg'
 
-class Post extends Component {
-  state = {
-    postObj: {}
-  }
-
-  componentWillReceiveProps(nextProps){
-    this.setState({postObj: nextProps.post})
-  }
-
-  render(){
+const Post = (props) => {
     return(
       <div>
-        <Link to={`/posts/${this.props.header === 'Readables!' ? 'All' : this.props.header}`}><img className="back-arrow" alt="A back arrow" src={backArrow}/></Link>
+        <Link to={`/posts/${props.header === 'Readables!' ? 'All' : props.header}`}><img className="back-arrow" alt="A back arrow" src={backArrow}/></Link>
         <div className="container">
           <div className="row post-body">
             <div className="col-sm-1 col-xs-2">
-              <img alt="An upvote arrow" className="arrow" src={arrow} onClick={ () => this.props.onVote(this.state.postObj.id, 'upVote')}/>
-              <h4 className="vote-number">{this.state.postObj.voteScore}</h4>
-              <img alt="A downvote arrow" className="arrow down-arrow" src={arrow} onClick={ () => this.props.onVote(this.state.postObj.id, 'downVote')}/>
+              <img alt="An upvote arrow" className="arrow" src={arrow} onClick={ () => props.onVote(props.post.id, 'upVote')}/>
+              <h4 className="vote-number">{props.post.voteScore}</h4>
+              <img alt="A downvote arrow" className="arrow down-arrow" src={arrow} onClick={ () => props.onVote(props.post.id, 'downVote')}/>
             </div>
             <div className="col-sm-9 col-xs-9">
-              <h4>{this.state.postObj.title}</h4>
-              <p>{this.state.postObj.body}</p>
+              <h4>{props.post.title}</h4>
+              <p>{props.post.body}</p>
               <center>
-                <p className="post-footer">{this.state.postObj.author}</p>
-                <p className="post-footer">{new Date(this.state.postObj.timestamp).toLocaleString()}</p>
+                <p className="post-footer">{props.post.author}</p>
+                <p className="post-footer">{new Date(props.post.timestamp).toLocaleString()}</p>
               </center>
             </div>
             <div className="col-sm-2">
-              <Link className="delete-post" to="/" onClick={() => this.props.onDeletePost(this.state.postObj.id)}>X</Link>
-              <Link to="/addnewpost" onClick={() => this.props.onGetPostId(this.state.postObj.id)}><img alt="edit symbol" className="edit-symbol" src={editSymbol}/></Link>
+              <Link className="delete-post" to="/" onClick={() => props.onDeletePost(props.post.id)}>X</Link>
+              <Link to="/addnewpost" onClick={() => props.onGetPostId(props.post.id)}><img alt="edit symbol" className="edit-symbol" src={editSymbol}/></Link>
             </div>
           </div>
-          {this.props.comments.map(comment => {
+          {props.comments.map(comment => {
             return (
               <div key={comment.id} className="row">
                 <div className="col-sm-11 col-xs-10 pull-right post-comment">
                   <div className="col-sm-1 col-xs-2">
-                    <img alt="An upvote arrow" className="arrow comment-arrow" src={arrow} onClick={() => this.props.onVoteComment(comment.id, 'upVote', this.state.postObj.id)}/>
+                    <img alt="An upvote arrow" className="arrow comment-arrow" src={arrow} onClick={() => props.onVoteComment(comment.id, 'upVote', props.post.id)}/>
                     <h4 className="vote-number">{comment.voteScore}</h4>
-                    <img alt="A downvote arrow" className="arrow down-arrow comment-arrow" src={arrow} onClick={() => this.props.onVoteComment(comment.id, 'downVote', this.state.postObj.id)}/>
+                    <img alt="A downvote arrow" className="arrow down-arrow comment-arrow" src={arrow} onClick={() => props.onVoteComment(comment.id, 'downVote', props.post.id)}/>
                   </div>
                   <div className="col-sm-10 col-xs-9">
                     <p>{comment.body}</p>
@@ -55,8 +46,8 @@ class Post extends Component {
                     </center>
                   </div>
                   <div className="col-xs-1">
-                    <Link to="/" className="delete-post"  onClick={() => this.props.onDeleteComment(comment.id, this.state.postObj.id)}>X</Link>
-                    <Link to="/addnewcomment" onClick={() => this.props.onGetCommentId(comment.id)}><img alt="edit symbol" className="edit-symbol" src={editSymbol}/></Link>
+                    <Link to="/" className="delete-post"  onClick={() => props.onDeleteComment(comment.id, props.post.id)}>X</Link>
+                    <Link to="/addnewcomment" onClick={() => props.onGetCommentId(comment.id)}><img alt="edit symbol" className="edit-symbol" src={editSymbol}/></Link>
                   </div>
                 </div>
               </div>
@@ -66,7 +57,6 @@ class Post extends Component {
         <Link to="/addnewcomment"><img alt="A plus sign to add a post" className="addcomment-img" src={addPostImg}/></Link>
       </div>
     )
-  }
 }
 
 export default Post
